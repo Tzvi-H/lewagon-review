fc39cef964f00a70f48935c9319f5dd95a8f837d
Tzvi-H <16guitar@gmail.com>
2022-04-24 17:41:57 -0500

    posts

--- hacker-news-sql/models/posts.rb
+++ hacker-news-sql/models/posts.rb
@@ -1,24 +1,37 @@
 require 'sqlite3'
 
-DB = SQLite3::Database.new("db/posts.db")
+DB = SQLite3::Database.new("../db/posts.db")
 DB.results_as_hash = true
 
 class Post
+  attr_accessor :id, :title, :url, :votes
+
+  def initialize(attributes)
+    @id = attributes[:id] || attributes["id"]
+    @title = attributes[:title] || attributes["title"]
+    @url = attributes[:url] || attributes["url"]
+    @votes = attributes[:votes] || attributes["votes"]
+  end
+
+  def destroy
+    DB.execute("delete from posts where id = ?", self.id)
+  end
+
+  def save
+    if self.id 
+      DB.execute("UPDATE posts SET title = ?, url = ?, votes = ? WHERE id = ?", self.title, self.url, self.votes, self.id)
+    else
+      DB.execute("INSERT INTO posts (title, url, votes) VALUES (?, ?, ?)", self.title, self.url, self.votes)
+      self.id = DB.last_insert_row_id
+    end
+  end
+
   def self.find(id)
-    results = DB.execute("select * from posts where id = ?", id)
-    results[0]
+    result = DB.execute("select * from posts where id = ?", id)[0]  
+    Post.new(result) if result
   end
 
   def self.all
     DB.execute("select * from posts")
   end
-
-  def initialize(attributes)
-    @id = attributes.id
-    @title = attributes.title
-    @url = attributes.url
-    @votes = attributes.votes
-  end
 end
-
-puts Post.all()
